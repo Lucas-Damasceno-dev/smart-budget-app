d33655a920a1d3ae737fbbc7e196dfca90ccbaee
Lucas Damasceno <lucas.damasceno.dev@gmail.com>
2025-12-25 08:50:02 -0300

    :bricks: ci(spring): initial construction of the spring structure

---
+++ src/main/java/com/lucas/damasceno/dev/gmail/com/smart_budget_app/SmartBudgetAppApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.damasceno.dev.gmail.com.smart_budget_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBudgetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBudgetAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/damasceno/dev/gmail/com/smart_budget_app/SmartBudgetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.damasceno.dev.gmail.com.smart_budget_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBudgetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
