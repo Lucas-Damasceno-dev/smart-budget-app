568c6831ba068736797138f738448c1fbb650db1
Lucas Damasceno <lucas.damasceno.dev@gmail.com>
2025-12-25 14:55:30 +0000

    :bricks: ci(system): initial basic directory structure

---
+++ src/test/java/com/lucasdamasceno/smartbudget/SmartBudgetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdamasceno.smartbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBudgetAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
